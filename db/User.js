3bd54a97c38ef21056377c71ea4c96663d503ebc
BigThinkerYes <designaspnet4@hotmail.com>
2023-02-23 12:14:26 -0600

    Delete User.js

--- db/User.js
+++
@@ -1,9 +0,0 @@
-const {Sequelize, sequelize} = require('./db');
-
-const User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  name: Sequelize.STRING,
-  password: Sequelize.STRING
-});
-
-module.exports = { User };
