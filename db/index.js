0b1454cc7061a78b2109f90a5efa81c81a442bba
BigThinkerYes <designaspnet4@hotmail.com>
2023-02-23 12:14:09 -0600

    Delete index.js

--- db/index.js
+++
@@ -1,9 +0,0 @@
-const {AdaJournal} = require('./AdaJournal');
-const {User} = require('./User');
-const {sequelize, Sequelize} = require('./db');
-
-module.exports = {
-    AdaJournal,User,
-    sequelize,
-    Sequelize
-};
