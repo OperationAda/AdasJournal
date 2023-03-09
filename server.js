9bbc43c7dc70105808803fbb369e95c73eca1355
Hannah Berhe <hannahbberhe@gmail.com>
2023-03-09 11:09:57 -0500

    authentication

--- server.js
+++ server.js
@@ -4,7 +4,23 @@ const { sequelize } = require("./db");
 const app = express();
 
 const port = 3000;
+const { auth } = require('express-openid-connect');
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'twtXWz4JujUq2YLcq70Gt0wECkAwfbPw',
+  issuerBaseURL: 'https://dev-yvei8zpyhh37mrs5.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 //TODO: Create your GET Request Route Below:
 const adas_journal_conn = require("./routes/adas_journal");
 app.use("/adas_journal", adas_journal_conn);
