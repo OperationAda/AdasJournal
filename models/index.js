27c2d14f9912ae28cf8b3929aa61ac9e51c1087f
WillV03 <candyman14920@gmail.com>
2023-03-09 09:44:52 -0500

    Added Model association between user and post

--- models/index.js
+++ models/index.js
@@ -1,4 +1,17 @@
+const Sequelize = require("sequelize");
+const {sequelize} = require("../db");
 const {AdasJournal} = require('./AdasJournal')
+const {User} = require('./User')
 
+User.associate = models => {
+    User.hasMany(models.AdasJournal, {
+    foreignKey: 'adaId'});
+}
 
+AdasJournal.associate = (models) => {
+  AdasJournal.belongsTo(models.User, {
+    foreignKey: 'adaId'});
+
+    
+    
 module.exports = {AdasJournal};
